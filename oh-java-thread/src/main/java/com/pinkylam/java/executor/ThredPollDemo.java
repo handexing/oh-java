c6b893f7abad64481aa6efd9be353be905c929e3
handexing <908716835@qq.com>
2017-11-15 20:46:04 +0800

    add executor

---
+++ oh-java-thread/src/main/java/com/pinkylam/java/executor/ThredPollDemo.java
@@ -0,0 +1,34 @@
+package com.pinkylam.java.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 固定大小的线程池
+ */
+public class ThredPollDemo {
+
+	public static class MyTask implements Runnable {
+
+		public void run() {
+			System.out.println(System.currentTimeMillis() + " Thread ID:" + Thread.currentThread().getId());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		MyTask task = new MyTask();
+		// 新建5个固定线程
+		ExecutorService es = Executors.newFixedThreadPool(5);
+		for (int i = 0; i < 10; i++) {
+			es.submit(task);
+		}
+
+	}
+
+}
